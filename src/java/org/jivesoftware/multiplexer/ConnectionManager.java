ab5aa7a8827abc8b592dbf50b99c686c71e62e0a
Gaston Dombiak <gato@jivesoftware.com>
2007-10-30 06:35:28 +0000

    Updated for 3.4.0 release.

--- src/java/org/jivesoftware/multiplexer/ConnectionManager.java
+++ src/java/org/jivesoftware/multiplexer/ConnectionManager.java
@@ -148,7 +148,7 @@ public class ConnectionManager {
         name = JiveGlobals.getXMLProperty("xmpp.manager.name", StringUtils.randomString(5)).toLowerCase();
         serverName = JiveGlobals.getXMLProperty("xmpp.domain");
 
-        version = new Version(3, 4, 0, Version.ReleaseStatus.Beta, 1);
+        version = new Version(3, 4, 0, Version.ReleaseStatus.Release, -1);
         if (serverName != null) {
             setupMode = false;
         }
