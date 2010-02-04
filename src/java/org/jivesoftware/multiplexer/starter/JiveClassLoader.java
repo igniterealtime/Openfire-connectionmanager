06246a73d2d7003fb58aa7a1a788600ddfc2d120
Guenther Niess <niess@uni-potsdam.de>
2010-02-04 10:40:25 +0000

    OF-46: Updating Jetty to 7.0.1.v20091125

--- src/java/org/jivesoftware/multiplexer/starter/JiveClassLoader.java
+++ src/java/org/jivesoftware/multiplexer/starter/JiveClassLoader.java
@@ -39,7 +39,7 @@ class JiveClassLoader extends URLClassLoader {
      * @throws java.net.MalformedURLException if the libDir path is not valid.
      */
     JiveClassLoader(ClassLoader parent, File libDir) throws MalformedURLException {
-        super(new URL[] { libDir.toURL() }, parent);
+        super(new URL[] { libDir.toURI().toURL() }, parent);
 
         File[] jars = libDir.listFiles(new FilenameFilter() {
             public boolean accept(File dir, String name) {
@@ -62,7 +62,7 @@ class JiveClassLoader extends URLClassLoader {
 
         for (int i = 0; i < jars.length; i++) {
             if (jars[i].isFile()) {
-                addURL(jars[i].toURL());
+                addURL(jars[i].toURI().toURL());
             }
         }
     }
